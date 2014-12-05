cf8a1bd90f44340fc006f439f43873d04ccdded2
Joseph North <joseph@pixelunion.net>
2014-12-04 16:38:58 -0800

    Add initial version.

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-jquery.trend
-============
+# jquery.trend
 
 Fail-safe TransitionEnd event for jQuery.
+
+Adds a new `trend` event that can be used in browsers that don't support
+`transitionend`.
+
+It works by parsing an element's `transitionDuration` property and using
+`setTimeout` as a fallback. Because of this, Trend can only be used with
+`jQuery.one`.
+
+## Usage
+
+```css
+.some-element {
+  opacity: 0;
+  transition: opacity 200ms;
+}
+
+.some-element.active {
+  opacity: 1;
+}
+```
+
+```js
+var el = $(".some-element");
+
+el.addClass("active");
+
+el.one("trend", function(e){
+  // Called approx 200ms later
+});
+```
---
+++ jquery.trend.js
@@ -0,0 +1,126 @@
+/*!
+ * Trend 0.1.0
+ *
+ * Fail-safe TransitionEnd event for jQuery.
+ *
+ * Adds a new "trend" event that can be used in browsers that don't
+ * support "transitionend".
+ *
+ * NOTE: Only supports being bound with "jQuery.one".
+ *
+ * Copyright 2014, Pixel Union - http://pixelunion.net
+ * Released under the MIT license
+ */
+;(function($){
+
+  // Prefixed transitionend event names
+  var transitionEndEvents =
+    "webkitTransitionEnd " +
+    "otransitionend " +
+    "oTransitionEnd " +
+    "msTransitionEnd " +
+    "transitionend";
+
+  // Prefixed transition duration property names
+  var transitionDurationProperties = [
+    "transition-duration",
+    "-moz-transition-duration",
+    "-webkit-transition-duration",
+    "-ms-transition-duration",
+    "-o-transition-duration",
+    "-khtml-transition-duration"
+  ];
+
+  // Parses a CSS duration value into milliseconds.
+  var parseDuration = function(s) {
+    s = s.replace(/\s/, "");
+    var v = window.parseFloat(s);
+
+    return s.match(/[^m]s$/i)
+      ? v * 1000
+      : v;
+  };
+
+  // Get the transition duration for an element, as specified by CSS.
+  // Returns a value in milliseconds.
+  var getTransitionDuration = function(el) {
+    var duration = 0;
+
+    for (var i = 0; i < transitionDurationProperties.length; i++) {
+      // Get raw CSS value
+      var value = el.css(transitionDurationProperties[i]);
+      if (!value) continue;
+
+      // Multiple transitions--pick the longest
+      if (value.indexOf(",") !== -1) {
+        var values = value.split(",");
+        var durations = (function(){
+          var results = [];
+          for (var i = 0; i < values.length; i++) {
+            var duration = parseDuration(values[i]);
+            results.push(duration);
+          }
+          return results;
+        })();
+
+        duration = Math.max.apply(Math, durations);
+      }
+
+      // Single transition
+      else {
+        duration = parseDuration(value);
+      }
+
+      // Accept first vaue
+      break;
+    }
+
+    return duration;
+  };
+
+  $.event.special.trend = {
+    // Triggers an event handler when an element is done transitioning.
+    //
+    // Handles browsers that don't support transitionend by adding a
+    // timeout with the transition duration.
+    add: function(handleObj) {
+      var el = $(this);
+      var fired = false;
+
+      // Mark element as being in transition
+      el.data("trend", true);
+
+      // Calculate a fallback duration. + 20 because some browsers fire
+      // timeouts faster than transitionend.
+      var duration = getTransitionDuration(el) + 20;
+
+      var cb = function(e) {
+        // transitionend events can be sent for each property. Let's just
+        // skip all but the first. Also handles the timeout callback.
+        if (fired) return;
+
+        // Child elements that also have transitions can be fired before we
+        // complete. This will catch and ignore those. Unfortunately, we'll
+        // have to rely on the timeout in these cases.
+        if (e && e.srcElement !== el[0]) return;
+
+        // Mark element has not being in transition
+        el.data("trend", false);
+
+        // Callback
+        fired = true;
+        if (handleObj.handler) handleObj.handler();
+      };
+
+      el.one(transitionEndEvents, cb);
+      el.data("trend-timeout", window.setTimeout(cb, duration));
+    },
+
+    remove: function(handleObj) {
+      var el = $(this);
+      el.off(transitionEndEvents);
+      window.clearTimeout(el.data("trend-timeout"));
+    }
+  };
+
+})(jQuery);
